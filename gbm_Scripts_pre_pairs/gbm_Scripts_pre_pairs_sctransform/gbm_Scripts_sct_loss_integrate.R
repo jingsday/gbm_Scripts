7b5c82b73c06d2798903f48565e49a0630e601a0
jingsday <lidiasday@gmail.com>
2025-02-27 19:55:35 +0100

    worked but need more hyperplane

--- gbm_Scripts_pre_pairs/gbm_Scripts_pre_pairs_sctransform/gbm_Scripts_sct_loss_integrate.R
+++ gbm_Scripts_pre_pairs/gbm_Scripts_pre_pairs_sctransform/gbm_Scripts_sct_loss_integrate.R
@@ -59,3 +59,57 @@ seurat.integrated <- IntegrateData(anchorset = anchors, normalization.method = "
 #Data ready to be retrieved here 
 saveRDS(seurat.integrated,"/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_project_integrated.rds") 
 head(seurat.integrated@meta.data)
+
+seu <- subset(seurat.integrated,subset = Sample == 'SF11082')  
+
+#Primary and recurrent
+
+metadata <-read.delim2('/home/jing/Phd_project/project_GBM/gbm_DATA/gbm_DATA_metadata/GSE174554_Tumor_normal_metadata_11916v2.txt',
+                      sep = " ")
+head(metadata)
+'SF11082_AAACCCAAGCATCAAA-1  '
+metadata$Barcode <- paste0(metadata$Sample, "_", metadata$Barcode, "-1")
+rownames(metadata) <- metadata$Barcode
+head(rownames(metadata))
+
+df <- metadata[metadata$Barcode %in% rownames(seurat.integrated@meta.data), ]
+rownames(df) <- df$Barcode
+seurat.integrated@meta.data$Tumor_Normal_annotation <- df[rownames(seurat.integrated@meta.data), "Tumor_Normal_annotation"]
+
+seurat.integrated@meta.data$Status <- 'Recurrent'
+
+# List of samples to change the 'Status' to 'Primary'
+primary_samples <- c('SF2770', 'SF2990', 'SF3076', 'SF3391', 'SF11916', 'SF11082', 'SF9358', 'SF9798')
+
+# Change 'Status' to 'Primary' for matching samples
+for (sample in primary_samples) {
+  seurat.integrated@meta.data$Status[grepl(sample, seurat.integrated@meta.data$Sample, fixed = TRUE)] <- 'Primary'
+}
+
+head(seurat.integrated@meta.data)
+
+# Retrieve df for Support vector machine 
+
+tumor_seu <- subset(seurat.integrated, subset = Tumor_Normal_annotation=='Tumor')
+tumor_seu_prm <- subset(tumor_seu, subset = Status=='Primary')
+tumor_seu_rct <- subset(tumor_seu, subset = Status=='Recurrent')
+
+prm_corrected_UMI <- tumor_seu_prm[["SCT"]]$data
+prm_corrected_UMI <-t(prm_corrected_UMI)
+
+writeMM(prm_corrected_UMI, "/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_OUTPUT_sct_tumor_prm_corrected_UMI.mtx")
+write.table(rownames(prm_corrected_UMI), 
+            file = "/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_OUTPUT_sct_tumor_prm_corrected_UMI_cells.txt", col.names = FALSE, row.names = FALSE)
+write.table(colnames(prm_corrected_UMI), 
+            file = "/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_OUTPUT_sct_tumor_prm_corrected_UMI_genes.txt", col.names = FALSE, row.names = FALSE)
+
+
+rct_corrected_UMI <- tumor_seu_rct[["SCT"]]$data
+rct_corrected_UMI <-t(rct_corrected_UMI)
+
+writeMM(rct_corrected_UMI, "/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_OUTPUT_sct_tumor_rct_corrected_UMI.mtx")
+write.table(rownames(rct_corrected_UMI), 
+            file = "/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_OUTPUT_sct_tumor_rct_corrected_UMI_cells.txt", col.names = FALSE, row.names = FALSE)
+write.table(colnames(rct_corrected_UMI), 
+            file = "/home/jing/Phd_project/project_GBM/gbm_OUTPUT_sctransform/gbm_OUTPUT_sct_tumor_rct_corrected_UMI_genes.txt", col.names = FALSE, row.names = FALSE)
+
